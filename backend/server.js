ead72d8a8fbf57509cd3613e75a6d603787fa756
Akash Sarkar <akashsar95@gmail.com>
2026-08-19 19:38:17 +0530

    Fixed server

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
-const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
@@ -12,13 +11,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ MongoDB Connected'))
     .catch(err => console.log('❌ Database Error:', err));
 
 app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
 app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
